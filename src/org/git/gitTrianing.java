9fd10affeee01c4ae18071f25c6dbbfb107906b2
viswanathnatarajan <viswan325@gmail.com>
2022-11-23 16:29:02 +0530

    java project A

---
+++ src/org/git/gitTrianing.java
@@ -0,0 +1,17 @@
+package org.git;
+
+import java.util.Scanner;
+
+public class gitTrianing {
+	public static void main(String[] args) {
+		System.out.println("sum of numbers");
+		int a=555;
+		int b=8888;
+		int c= a+b;
+		System.out.println(c);
+		System.out.println("end>>>>>>>>>>>>>");
+		
+		
+	}
+
+}
