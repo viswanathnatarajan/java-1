260fd880c88d24d0c6e4c9f8ffb338d18f269daa
viswanathnatarajan <viswan325@gmail.com>
2022-11-24 15:33:25 +0530

    A add some codes

--- src/org/git/gitTrianing.java
+++ src/org/git/gitTrianing.java
@@ -1,17 +1,23 @@
 package org.git;
-
-import java.util.Scanner;
-
 public class gitTrianing {
+	 
 	public static void main(String[] args) {
 		System.out.println("sum of numbers");
 		int a=555;
 		int b=8888;
 		int c= a+b;
+		int d = a-b;
+		int e = a/b;
 		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
 		System.out.println("end>>>>>>>>>>>>>");
 		
 		
 	}
+		
+		
 
-}
+	}
+
+
